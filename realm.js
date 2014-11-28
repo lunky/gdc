aac7d1582fafae6e3ee8333b3e5fc1796d51d812
Quinn Wilson <git@midword.org>
2014-11-27 19:34:33 -0600

    forgot to add realm

--- realm.js
+++ realm.js
@@ -1,11 +1,15 @@
 (function(){
 	var Realm = function(){
 		exports = {
+			Add : Add ,
 			Tick : Tick
-		}
+		};
+		function Add(cell){
+			console.log("Add");	
+		};
 		function Tick(){
 			console.log("Tick");
-		}
+		};
 		return exports;
 	};
 	// conways game of life
