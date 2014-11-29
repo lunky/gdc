764e479c50747bfe6b221500e80cd81bea26b773
lunky <github@midworld.org>
2014-11-29 00:31:23 -0600

    get rid of assert usage

--- test/realm_tests.js
+++ test/realm_tests.js
@@ -1,19 +1,4 @@
 (function(){
-/*
-	var assert = require("assert")
-
-	assert.fail(actual, expected, message, operator)
-	assert(value, message), assert.ok(value, [message])
-	assert.equal(actual, expected, [message])
-	assert.notEqual(actual, expected, [message])
-	assert.deepEqual(actual, expected, [message])
-	assert.notDeepEqual(actual, expected, [message])
-	assert.strictEqual(actual, expected, [message])
-	assert.notStrictEqual(actual, expected, [message])
-	assert.throws(block, [error], [message])
-	assert.doesNotThrow(block, [message])
-	assert.ifError(value)
-*/
 	var assert  = require("assert");
 	var sinon = require("sinon");
 
